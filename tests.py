54d0b7af74c344676546332ec0f79b71abfb4db9
bonchardon <61079942+bonchardon@users.noreply.github.com>
2021-08-23 16:52:52 +0300

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,21 @@
+from code import DataRetriever, Analyzer
+import unittest
+
+
+class TestPreprocessing(unittest.TestCase):
+    def test_clean_tweet(self):
+        # initialize DataRetriever
+        retr = DataRetriever()
+        retr.raw_data = retr.testdata
+
+        # initialize Analyzer
+        analy = Analyzer(retr)
+
+        # preprocess tweets
+        result = analy._clean_tweet("I was lost")
+
+        self.assertEqual(result, "lost")
+
+
+if __name__ == '__main__':
+    unittest.main()
